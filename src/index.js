31335df50feaaaffe3709a5fd1e5a9a6662ba74a
Sheldon <shsram@gmail.com>
2026-05-23 15:31:30 -0400

    Fix index.js import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-js
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(App, null)
+);
