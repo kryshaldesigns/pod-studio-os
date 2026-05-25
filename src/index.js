d6da473f9a118569c6ce787877125e5bfae5809f
Sheldon <shsram@gmail.com>
2026-05-24 22:51:26 -0400

    Restore CRA build - correct working setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(App, null)
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
